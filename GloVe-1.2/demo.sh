54e84770f192d97463455a95e5c5a710af96cadd
HYC <yunchaohe@gmail.com>
2016-01-07 23:56:56 +0800

    add GloVe tools

--- GloVe-1.2/demo.sh
+++ GloVe-1.2/demo.sh
@@ -2,10 +2,13 @@
 
 # Makes programs, downloads sample data, trains a GloVe model, and then evaluates it.
 # One optional argument can specify the language used for eval script: matlab, octave or [default] python
-
+# 1.vocab_count:用于计算原文本的单词统计（生成vocab.txt,每一行为：单词  词频）
+# 2.cooccur：用于统计词与词的共现，目测类似与word2vec的窗口内的任意两个词（生成的是cooccurrence.bin,二进制文件，呵呵）
+# 3. shuffle：对于2中的共现结果重新整理（一看到shuffle瞬间想到hadoop，生成的也是二进制文件cooccurrence.shuf.bin）
+# 4.glove：glove算法的训练模型，会运用到之前生成的相关文件（1&3），最终会输出vectors.txt和vectors.bin（前者直接可以打开，下文主要针对它做研究，后者还是二进制文件）
 make
 
-CORPUS=/home/hs/Data/wikipedia/wiki.zh.text.simplified_seg
+CORPUS=/home/hs/Data/wikipedia/wiki.zh.text.traditional_seg
 VOCAB_FILE=vocab.txt
 COOCCURRENCE_FILE=cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
