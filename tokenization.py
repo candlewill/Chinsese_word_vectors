9ca59427efc48eb54775749682e3c2b00b509e15
HYC <yunchaohe@gmail.com>
2016-01-07 16:16:53 +0800

    分词以及测试文件

---
+++ tokenization.py
@@ -0,0 +1,19 @@
+import time
+import jieba
+
+jieba.enable_parallel(8)
+
+input = '/home/hs/Data/wikipedia/wiki.zh.text.simplified'
+output = '/home/hs/Data/wikipedia/wiki.zh.text.simplified_seg'
+
+content = open(input,"rb").read()
+t1 = time.time()
+words = " ".join(jieba.cut(content))          # 精确模式
+
+t2 = time.time()
+tm_cost = t2-t1
+
+log_f = open(output,"wb")
+log_f.write(words.encode('utf-8'))
+
+print('speed %s bytes/second' % (len(content)/tm_cost))
